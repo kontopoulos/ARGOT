637f349c9ca11b0ebb145d2a11dc6c337709a46d
ikon <ikon@iit.demokritos.gr>
2016-02-25 20:11:18 +0200

    sentence splitter updated

--- ParallelNGG/src/main/java/Starter.scala
+++ ParallelNGG/src/main/java/Starter.scala
@@ -11,19 +11,23 @@ object Starter {
       .setMaster("local[*]")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .set("spark.kryoserializer.buffer","64mb")
-      .registerKryoClasses(Array(classOf[MergeOperator], classOf[IntersectOperator], classOf[InverseIntersectOperator], classOf[DeltaOperator], classOf[GraphSimilarityCalculator], classOf[StringEntityTokenizer]))
+      .registerKryoClasses(Array(classOf[MergeOperator], classOf[IntersectOperator], classOf[InverseIntersectOperator], classOf[DeltaOperator], classOf[GraphSimilarityCalculator], classOf[StringEntityTokenizer], classOf[OpenNLPSentenceSplitter]))
       .set("spark.executor.memory", "2g")
     val sc = new SparkContext(conf)
 
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-
     val start = System.currentTimeMillis
 
     /*val dec = new DocumentEventClustering(sc)
     val clusters = dec.getClusters("corpora")
     dec.saveClustersToCsv(clusters)*/
+
+    val e = new StringEntity
+    e.readFile(sc, "corpora/0001829", 2)
+    val ss = new OpenNLPSentenceSplitter("en-sent.bin")
+    ss.getSentences(e).foreach(a => println("Beg:" + a.dataStream))
 
 
     val end = System.currentTimeMillis
--- ParallelNGG/src/main/java/nlp/OpenNLPSentenceSplitter.scala
+++ ParallelNGG/src/main/java/nlp/OpenNLPSentenceSplitter.scala
@@ -6,7 +6,7 @@ import org.apache.spark.rdd.RDD
 /**
   * @author Kontopoulos Ioannis
   */
-class OpenNLPSentenceSplitter(val modelFile: String) extends SentenceSplitter {
+class OpenNLPSentenceSplitter(val modelFile: String) extends SentenceSplitter with Serializable {
 
   /**
     * Given an entity return an RDD containing its sentences
@@ -15,9 +15,25 @@ class OpenNLPSentenceSplitter(val modelFile: String) extends SentenceSplitter {
     */
   override def getSentences(e: Entity): RDD[Atom] = {
     val en = e.asInstanceOf[StringEntity]
-    en.dataStringRDD.glom.map(_.mkString(" "))
+    //split string of each partition to sentences
+    val badSentences = en.dataStringRDD.glom.map(_.mkString(" ").replaceAll(" +", " "))
       .flatMap(text => getDetectedSentences(text))
-      .map(sentence => new StringAtom(sentence.hashCode, sentence))
+    //the text is originally split into lines and these lines spread to the partitions (that`s how spark reads a file)
+    //sentences of the text might consist of more than one lines
+    //because a sentence might be split to more than one partitions
+    //we take the sentences that were not split to partitions
+    val goodSplits = badSentences
+        .filter(s => (s.endsWith(".") || s.endsWith("?") || s.endsWith("!") || s.endsWith(":") || s.endsWith(";")) && !Character.isLowerCase(s.codePointAt(0)))
+    //we take sentences that were split to partitions
+    val badSplits = badSentences
+      .filter(s => (!s.endsWith(".") && !s.endsWith("?") && !s.endsWith("!") && !s.endsWith(":") && !s.endsWith(";")) || !Character.isUpperCase(s.codePointAt(0)))
+      //we move them to one partition, create a single string and we split to sentences
+      //even in a huge cluster the sentences that were split are not many
+      //so moving them to one partition does not have a huge impact on the memory of that partition
+      .coalesce(1).glom.map(_.mkString(" "))
+      .flatMap(text => getDetectedSentences(text))
+    //we union the good sentences with the partitioned ones and take the distinct
+    goodSplits.union(badSplits).distinct.map(s => new StringAtom(s.hashCode, s))
   }
 
   /**
