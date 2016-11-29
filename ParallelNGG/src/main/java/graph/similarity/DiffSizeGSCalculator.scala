bf2902ef7a8f33247fd01354f7199c5ea14bc012
Ioannis Kon <ioannis-kon@outlook.com>
2016-11-29 23:27:39 +0200

    diff size gscalculator optimized

--- ParallelNGG/src/main/java/graph/similarity/DiffSizeGSCalculator.scala
+++ ParallelNGG/src/main/java/graph/similarity/DiffSizeGSCalculator.scala
@@ -24,24 +24,24 @@ class DiffSizeGSCalculator(sc: SparkContext) {
     // calculate size similarity
     val sSimil = Math.min(smallEdgeCount, largeEdgeCount).toDouble/Math.max(smallEdgeCount, largeEdgeCount)
     // map edges to key/value pairs and broadcast edges of small graph to the cluster
-    val smallGraphEdges = sc.broadcast(smallGraph.edges.toMap)
+    val smallGraphEdges = sc.broadcast(smallGraph.edges)
     // map edges of the large graph to key/value pairs
     val largeGraphEdges = dGraph.edges.map(e => ((e.srcId, e.dstId), e.attr))
     // extract the common edges of the graphs
     val commonEdges = largeGraphEdges
       // take edges that exist in both graphs
       .filter(e => smallGraphEdges.value.contains((e._1._1,e._1._2)))
+      // now each partition is too small, so there is no need for distribution
+      .collect
       // map edges to key/value pairs where the value is a tuple of both edge weights
-      .map(e => ((e._1),(e._2,smallGraphEdges.value(e._1))))
-    commonEdges.cache
-    // c holds the number of common edges
-    val commonEdgesCount = commonEdges.count
+      .map(e => ((e._1),(e._2,smallGraph.edges(e._1))))
+    // the variable below holds the number of common edges
+    val commonEdgesCount = commonEdges.length
     var vSimil = 0.0
     // if there are common edges
     if (commonEdgesCount != 0) {
       vSimil = commonEdges.map(e => Math.min(e._2._1, e._2._2)/Math.max(e._2._1, e._2._2)).sum/Math.max(smallEdgeCount,largeEdgeCount)
     }
-    commonEdges.unpersist()
     // for each common edge add 1/min to a sum
     val cSimil = (1.toDouble/Math.min(smallEdgeCount, largeEdgeCount))*commonEdgesCount
     val gs = new GraphSimilarity(sSimil, vSimil, cSimil)
